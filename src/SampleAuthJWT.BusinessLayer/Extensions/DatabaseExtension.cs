a519b833f0757d05d9575f00de0bc441b1adccfe
Angelo Pirola <angelo9871@gmail.com>
2024-10-18 10:34:31 +0200

    Aggiunta class library Business Layer

---
+++ src/SampleAuthJWT.BusinessLayer/Extensions/DatabaseExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using SampleAuthJWT.DataAccessLayer;
+
+namespace SampleAuthJWT.BusinessLayer.Extensions;
+
+public static class DatabaseExtension
+{
+    public static async Task ConfigureDatabaseAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await EnsureDatabaseAsync(dbContext);
+        await RunMigrationsAsync(dbContext);
+    }
+
+    private static async Task EnsureDatabaseAsync(ApplicationDbContext dbContext)
+    {
+        var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+
+        await strategy.ExecuteAsync(async () =>
+        {
+            // Create the database if it does not exist.
+            // Do this first so there is then a database to start a transaction against.
+            if (!await dbCreator.ExistsAsync())
+            {
+                await dbCreator.CreateAsync();
+            }
+        });
+    }
+
+    private static async Task RunMigrationsAsync(ApplicationDbContext dbContext)
+    {
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+
+        await strategy.ExecuteAsync(async () =>
+        {
+            // Run migration in a transaction to avoid partial migration if it fails.
+            await using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+            await dbContext.Database.MigrateAsync();
+            await transaction.CommitAsync();
+        });
+    }
+}
