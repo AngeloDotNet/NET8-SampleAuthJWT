951d94652805834e4611afa264595b987fdf9e5a
Angelo Pirola <angelo9871@gmail.com>
2024-10-30 09:43:13 +0100

    Refactoring BusinessLayer

--- src/SampleAuthJWT.BusinessLayer/Extensions/APIExtension.cs
+++ src/SampleAuthJWT.BusinessLayer/Extensions/APIExtension.cs
@@ -22,7 +22,52 @@ public static class APIExtension
         return result.Succeeded ? TypedResults.Created() : TypedResults.BadRequest(result.Errors);
     }
 
-    public static async Task<IResult> LoginQrCodeAsync(LoginRequest request, SignInManager<ApplicationUser> signInManager,
+    public static async Task<IResult> LoginAsync(LoginRequest loginRequest, SignInManager<ApplicationUser> signInManager, DateTime? expiration,
+        IJwtBearerService jwtBearerService)
+    {
+        // Check for login rights...
+        var signInResult = await signInManager.PasswordSignInAsync(loginRequest.UserName, loginRequest.Password, false, false);
+
+        if (!signInResult.Succeeded)
+        {
+            return TypedResults.BadRequest(new AuthResponse(null, null, ["Invalid login request"]));
+        }
+
+        // Add custom claims (optional).
+        var claims = new List<Claim>();
+
+        if (loginRequest.Scopes?.Any() ?? false)
+        {
+            claims.Add(new("scp", loginRequest.Scopes));
+        }
+
+        // Manca la generazione del refresh token ??
+        var token = await jwtBearerService.CreateTokenAsync(loginRequest.UserName, claims, absoluteExpiration: expiration);
+
+        return TypedResults.Ok(new LoginResponse(token));
+    }
+
+    public static async Task<IResult> ValidateTokenAsync(string token, bool validateLifetime, IJwtBearerService jwtBearerService)
+    {
+        var result = await jwtBearerService.TryValidateTokenAsync(token, validateLifetime);
+
+        if (!result.IsValid)
+        {
+            return TypedResults.BadRequest();
+        }
+
+        return TypedResults.Ok(new User(result.Principal.Identity!.Name));
+    }
+
+    public static async Task<IResult> RefreshTokenAsync(string token, bool validateLifetime, DateTime? expiration,
+        IJwtBearerService jwtBearerService)
+    {
+        var newToken = await jwtBearerService.RefreshTokenAsync(token, validateLifetime, expiration);
+
+        return TypedResults.Ok(new LoginQrCodeResponse(newToken));
+    }
+
+    public static async Task<IResult> LoginQrCodeAsync(LoginQrCodeRequest request, SignInManager<ApplicationUser> signInManager,
         IJwtBearerService jwtBearerService, ITimeLimitedDataProtector dataProtector)
     {
         var user = await signInManager.UserManager.FindByEmailAsync(request.Email);
@@ -78,7 +123,7 @@ public static class APIExtension
         return TypedResults.File(qrCodeBytes, MediaTypeNames.Image.Png);
     }
 
-    public static async Task<IResult> ValidateQrCodeAsync(ValidationRequest request, ITimeLimitedDataProtector dataProtector,
+    public static async Task<IResult> ValidateQrCodeAsync(ValidationQrCodeRequest request, ITimeLimitedDataProtector dataProtector,
         UserManager<ApplicationUser> userManager, IJwtBearerService jwtBearerService)
     {
         ApplicationUser? user = null;
